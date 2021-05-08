bcf3067b92d394b864a2bcd7a6335adfa74f1f6d
Anshul21-coder <83653522+Anshul21-coder@users.noreply.github.com>
2021-05-08 10:35:33 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Calculator {
+	public static void main(String[] args) {
+
+		int num1, num2, res, i;
+		char op;
+
+		for(i = 0; i <= 15; i++)
+		{
+			System.out.print("Enter first number: ");
+			Scanner scan = new Scanner(System.in);
+			num1 = scan.nextInt();
+
+			System.out.print("Enter second number: ");
+			Scanner sc = new Scanner(System.in);
+			num2 = sc.nextInt();
+
+			System.out.print("Enter the operation: ");
+			Scanner in = new Scanner(System.in);
+			op = in.next().charAt(0);
+
+			switch(op)
+			{
+				case '+':
+				res = num1 + num2;
+				System.out.println(res);
+				break;
+
+				case '-':
+				res = num1 - num2;
+				System.out.println(res);
+				break;
+
+				case '*':
+				res = num1 * num2;
+				System.out.println(res);
+				break;
+
+				case '/':
+				res = num1 / num2;
+				System.out.println(res);
+				break;
+			}
+		}
+	}
+}
